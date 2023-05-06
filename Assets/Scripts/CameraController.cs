91b1c5afb2a4183488310de614852727f6c5552d
Necronous <97684040+Necronous@users.noreply.github.com>
2023-05-06 02:17:16 +0100

    Revert "Added SystemReference.txt"
    This reverts commit 70fd5cc000c61f517cf78186043229c65eca5e1b.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,14 +9,10 @@ public class CameraController : MonoBehaviour
     public static CameraController Main { get; private set; }
 
     private StateMachine _stateMachine = new();
-    private Camera _camera;
 
     private Transform _target;
 
     private Action _fadeEndCallback;
-    private Color _fadeColor;
-    private float _fadeEndTime;
-    private bool _isFading;
 
     #region StateMachinePassThrough
     public int CurrentState => _stateMachine.CurrentState;
@@ -45,13 +41,11 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _camera = GetComponent<Camera>();
-
         RegisterState((int)CameraModeEnum.FOLLOW_OBJECT, Follow_Object);
         //RegisterState((int)CameraModeEnum.FADE_IN, Fade_In);
         //RegisterState((int)CameraModeEnum.FADE_OUT, Fade_Out);
-
-        StartFollowingTarget();
+        
+        SetState((int)CameraModeEnum.FOLLOW_OBJECT);
     }
 
     private void FixedUpdate()
@@ -59,35 +53,17 @@ public class CameraController : MonoBehaviour
         _stateMachine.UpdateMachine();
     }
 
-
-    #region FollowObject
     public void SetTarget(Transform transform)
     {
         _target = transform; 
     }
-
-    public void StartFollowingTarget()
-    {
-        SetState((int)CameraModeEnum.FOLLOW_OBJECT);
-    }
-
-    private bool Follow_Object()
-    {
-        if (_target == null)
-            return true;
-        transform.position = _target.position;
-        return true;
-    }
-
-    #endregion
-    #region Fading
 
     /// <summary>
     /// Starts a fade in
     /// </summary>
     /// <param name="fadetime">How long the fade will take.</param>
     /// <param name="callback">Function to call once fade has ended.</param>
-    public void StartFadeIn(float fadetime, Color fadecolor, Action callback = null)
+    public void StartFadeIn(float fadetime = 1, Action callback = null)
     {
         throw new NotImplementedException();
     }
@@ -96,18 +72,29 @@ public class CameraController : MonoBehaviour
     /// </summary>
     /// <param name="fadetime">How long the fade will take.</param>
     /// <param name="callback">Function to call once fade has ended.</param>
-    public void StartFadeOut(float fadetime, Color fadecolor, Action callback = null)
+    public void StartFadeOut(float fadetime = 1, Action callback = null)
     {
         throw new NotImplementedException();
     }
 
+    #region FollowObject
+
+    private bool Follow_Object()
+    {
+        transform.position = _target.position;
+        return true;
+    }
+
+    #endregion
+    #region Fading
+
     private bool Fade_Out()
     {
-        throw new NotImplementedException();
+        return true;
     }
     private bool Fade_In()
     {
-        throw new NotImplementedException();
+        return true;
     }
 
     #endregion
